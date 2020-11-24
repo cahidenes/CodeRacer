a20301ca3742e235e3e12ee6d4699a11e7399a34
cahidenes <cahideneskeles54@gmail.com>
2020-11-24 18:05:48 +0300

    Fullscreen mode

--- desktop/src/com/cenes/coderacer/desktop/DesktopLauncher.java
+++ desktop/src/com/cenes/coderacer/desktop/DesktopLauncher.java
@@ -7,9 +7,9 @@ import com.cenes.coderacer.Main;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 16*90;
-		config.height = 9*90;
-//		config.fullscreen = true;
+		config.width = 1920;//16*90;
+		config.height = 1080;//9*90;
+		config.fullscreen = true;
 		new LwjglApplication(new Main(), config);
 	}
 }
